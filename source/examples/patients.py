6641a2516d9a45056dc85dc4b4bed70344a1d472
Peter Goodspeed-Niklaus <peter@bauxy.com>
2016-04-29 16:37:48 +0200

    Update patients for create with only email and automatic user.

--- source/examples/patients.py
+++ source/examples/patients.py
@@ -1,16 +1,14 @@
 from util import *
-#!SNIP START
+# !SNIP START
 response = requests.get(API_ROOT + 'patients/8/', auth=user)
 patient = response.json()
 printj(patient)
-#!SNIP END
+# !SNIP END
 print dump(response).decode('utf-8')
 
 # Post patient
 patient = {
-    'first_name' : 'Adam',
-    'last_name'  : 'Baker',
-    'email'      : 'ab@c.de'
+    'email': 'ab@c.de'
 }
 response = requests.post(API_ROOT + 'patients/', data=patient)
 printj(response.json())
